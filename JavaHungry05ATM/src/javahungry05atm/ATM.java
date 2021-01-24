890d1dd6800652847e0ad675a5fa812b7f223c87
Castro <luisc@Castro-Desktop>
2021-01-24 21:05:40 +0000

    edit withdrawFunds()

--- JavaHungry05ATM/src/javahungry05atm/ATM.java
+++ JavaHungry05ATM/src/javahungry05atm/ATM.java
@@ -207,7 +207,7 @@ public class ATM {
         //get the account to transfer from
         do {
             System.out.printf("Enter the number (1-%d) of the account to transfer\n"
-                    + " from: ");
+                    + " from: ", theUser.numAccounts());
             fromAccount = in.nextInt() - 1;
             if (fromAccount < 0 || fromAccount >= theUser.numAccounts()) {
                 System.out.println("Invalid account. Please try again.");
